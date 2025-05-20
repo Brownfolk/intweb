653bc875b037d4114a9034762ec78e1a2d050db5
Constantine <142466271+GORWhitewolF@users.noreply.github.com>
2025-05-20 14:29:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 function calc(){
     var sft=document.getElementById("input")
+    var floor=document.getElementById("input2")
 var sftvalue=Number(sft.value)
+var floorvalue=Number(floor.value)
 var price=1400;
+
 var display=document.getElementById("price")
-    var budget=sftvalue*price
-display.textContent="Rs "+budget+"/-"
+if(floorvalue==0){
+    display.textContent="Enter no of floors"
+}
+ else{var budget=sftvalue*floorvalue*price
+display.textContent="Rs "+budget+"/-"}   
 }
